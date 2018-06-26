40ab77f60ee46384c5aaa71b3f0c884d863887b9
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-06-26 22:01:48 +0800

    First-time Commit

---
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/shiro/api/model/VinsRealm2.java
@@ -0,0 +1,62 @@
+package application.io.spring.shiro.api.model;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * 	This is a class to customize a realm for authentication
+ * 
+ * @author vinsy
+ *
+ */
+public class VinsRealm2 implements Realm {
+
+	/**
+	 * 	This is a method to get the name of this realm
+	 */
+	@Override
+	public String getName() {
+		return "Vins Realm";
+	}
+
+	/**
+	 * 	This is a method to specify what type of token is supported by this realm
+	 */
+	@Override
+	public boolean supports(AuthenticationToken token) {
+		if (token instanceof UsernamePasswordToken) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 	This is a method to implement how a token should be authenticated
+	 * 	-- This token is the one from "subject.login(token)"
+	 */
+	@Override
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		
+		// Get principal and credentials from the token
+		String principal = (String) token.getPrincipal();
+		String credentials = new String((char[])token.getCredentials());
+		
+		// If principal and credentials are correct
+		if (principal.equals("vins") && credentials.equals("abc")) {
+			
+			// Return the authentication information
+			return new SimpleAuthenticationInfo(principal + "@gmail.com", credentials, getName());
+			
+		// If either principal or credentials is incorrect
+		} else {
+			
+			// Throw authentication exception
+			throw new AuthenticationException("principal or credentials incorrect");
+		}
+	}
+}
--- SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/shiro/gateway/web/ShiroController.java
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/shiro/gateway/web/ShiroController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,12 +67,12 @@ public class ShiroController {
 			currentUser.login(userToken);
 			
 			// Get the principal
-			Object currentPrincipal = currentUser.getPrincipal();
-			result.put("loginMsg", currentPrincipal + " has logged-in");
+			PrincipalCollection principals = currentUser.getPrincipals();
+			result.put("loginMsg", principals + " has logged-in");
 			
 			// Logout current user
 			currentUser.logout();
-			result.put("logoutMsg", currentPrincipal + " has logged-out");
+			result.put("logoutMsg", principals + " has logged-out");
 			
 			// Return data
 			data.put("status", 1);
@@ -123,13 +125,72 @@ public class ShiroController {
 			// Login current user with the token
 			currentUser.login(userToken);
 			
+			// Get the principals
+			PrincipalCollection principals = currentUser.getPrincipals();
+			result.put("loginMsg", principals + " has logged-in");
+
+			// Logout current user
+			currentUser.logout();
+			result.put("logoutMsg", principals + " has logged-out");
+			
+			// Return data
+			data.put("status", 1);
+			data.put("msg", "authentication succeeds");
+			data.put("result", result);
+			return data;
+			
+		// Exceptionally
+		} catch (Exception e) {
+			
+			// Return data
+			result.put("errMsg", e.getMessage());
+			data.put("status", -1);
+			data.put("msg", "authentication fails");
+			data.put("result", result);
+			return data;
+		}
+	}
+	
+	@RequestMapping(value = "/testAuthenticationWithAuthenticatorAndAuthenticationStrategy", method = RequestMethod.GET)
+	public Map<String, Object> testAuthenticationWithAuthenticatorAndAuthenticationStrategy() throws Exception {
+		
+		Map<String, Object> data = new HashMap<>();
+		Map<String, Object> result = new HashMap<>();
+		
+		/*
+		 * 	Get the principal and credential
+		 * 	-- Principal can be user-name, phone-number, email, etc.
+		 * 	-- Credential can be password, sign, digital-certificate, etc.
+		 */
+		String principal = request.getParameter("username");
+		String credentials = request.getParameter("password");
+		
+		// Get a security manager
+		SecurityManager securityManager = new IniSecurityManagerFactory("classpath:shiro/shiro-authenticator-and-authentication-strategy.ini")
+				.getInstance();
+		
+		// Set the security manager to the utility class
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		// Get current user from the utility class
+		Subject currentUser = SecurityUtils.getSubject();
+		
+		// Construct a token with principal and credential
+		UsernamePasswordToken userToken = new UsernamePasswordToken(principal, credentials);
+		
+		// Normally
+		try {
+			
+			// Login current user with the token
+			currentUser.login(userToken);
+			
 			// Get the principal
-			Object currentPrincipal = currentUser.getPrincipal();
-			result.put("loginMsg", currentPrincipal + " has logged-in");
+			PrincipalCollection principals = currentUser.getPrincipals();
+			result.put("loginMsg", principals + " has logged-in");
 			
 			// Logout current user
 			currentUser.logout();
-			result.put("logoutMsg", currentPrincipal + " has logged-out");
+			result.put("logoutMsg", principals + " has logged-out");
 			
 			// Return data
 			data.put("status", 1);
