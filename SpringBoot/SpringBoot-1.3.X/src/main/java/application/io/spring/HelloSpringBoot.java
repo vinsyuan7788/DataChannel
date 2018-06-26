4cddb49e7dda39d8170c78749d357be4c1946271
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-06-26 17:43:57 +0800

    First-time Commit

--- SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/HelloSpringBoot.java
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/HelloSpringBoot.java
@@ -1,7 +1,18 @@
 package application.io.spring;
 
+/**
+ * 	This is a class to test Maven project
+ * 
+ * @author vinsy
+ *
+ */
 public class HelloSpringBoot {
 	
+	/**
+	 * 	This is a main method for execution
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello SpringBoot!");
 	}
--- SpringCloud/SpringCloud-Brixton/src/main/java/application/io/spring/HelloSpringCloud.java
+++ SpringCloud/SpringCloud-Brixton/src/main/java/application/io/spring/HelloSpringCloud.java
@@ -1,7 +1,18 @@
 package application.io.spring;
 
+/**
+ * 	This is a class to test Maven project
+ * 
+ * @author vinsy
+ *
+ */
 public class HelloSpringCloud {
 	
+	/**
+	 * 	This is a main method for execution
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello SpringCloud!");
 	}
