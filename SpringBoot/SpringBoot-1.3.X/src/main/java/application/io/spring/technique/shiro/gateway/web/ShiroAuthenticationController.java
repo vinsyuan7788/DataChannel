6c1e90f203d5ec9914627eedbbb1d61cbf5db232
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-06-27 17:01:03 +0800

    First-time Commit

--- SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/gateway/web/ShiroAuthenticationController.java
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/gateway/web/ShiroAuthenticationController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -183,6 +184,96 @@ public class ShiroAuthenticationController {
 			result.put("errMsg", loginInfo.getMsg());
 			data.put("status", -1);
 			data.put("msg", "authentication fails");
+			data.put("result", result);
+			return data;
+		}
+	}
+	
+	@RequestMapping(value = "/testLogin", method = RequestMethod.GET)
+	public Map<String, Object> testLogin() throws Exception {
+		
+		Map<String, Object> data = new HashMap<>();
+		Map<String, Object> result = new HashMap<>();
+		
+		// Get the principal and credential
+		String principal = request.getParameter("username");
+		String credentials = request.getParameter("password");
+		
+		// Do the Shiro login and get the login information
+		LoginInfo loginInfo = ShiroUtils.login("shiro/authentication/shiro.ini", principal, credentials);
+		
+		// If login succeeds
+		if (loginInfo.getIsLogin().booleanValue() == true) {
+			
+			// Get the subject
+			Subject currentUser = loginInfo.getSubject();
+			
+			// Get the principal
+			PrincipalCollection principals = currentUser.getPrincipals();
+			result.put("loginMsg", principals + " has logged-in");
+			
+			// Return data
+			data.put("status", 1);
+			data.put("msg", "authentication succeeds");
+			data.put("result", result);
+			return data;
+			
+		// If login fails
+		} else {
+			
+			// Return data
+			result.put("errMsg", loginInfo.getMsg());
+			data.put("status", -1);
+			data.put("msg", "authentication fails");
+			data.put("result", result);
+			return data;
+		}
+	}
+	
+	@RequestMapping(value = "/testLogout", method = RequestMethod.GET)
+	public Map<String, Object> testLogout() throws Exception {
+		
+		Map<String, Object> data = new HashMap<>();
+		Map<String, Object> result = new HashMap<>();
+		
+		// Get the subject
+		Subject currentUser = SecurityUtils.getSubject();
+		
+		// If subject is not null
+		if (currentUser != null) {
+		
+			// Store some information
+			result.put("principals", currentUser.getPrincipals());
+			result.put("isRemembered", currentUser.isRemembered());
+			
+			// if subject is authenticated
+			if (currentUser.isAuthenticated()) {
+				
+				// Log the subject out
+				currentUser.logout();
+			
+				// Return data
+				data.put("status", 1);
+				data.put("msg", "success");
+				data.put("result", result);
+				return data;
+				
+			// If subject is not authenticated
+			} else {
+				
+				// Return data
+				data.put("status", -1);
+				data.put("msg", "subject not authenticated");
+				data.put("result", result);
+				return data;
+			}
+			
+		// If subject is null
+		} else {
+			
+			// Return data
+			data.put("status", -1);
+			data.put("msg", "subject not existed");
 			data.put("result", result);
 			return data;
 		}
