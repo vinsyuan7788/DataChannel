9b477008c5f339e01c4117fe7827b52dcb9a372f
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-06-27 16:23:54 +0800

    First-time Commit

---
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/gateway/web/ShiroSessionController.java
@@ -0,0 +1,90 @@
+package application.io.spring.technique.shiro.gateway.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.io.spring.technique.shiro.api.model.utils.LoginInfo;
+import application.io.spring.utils.ShiroUtils;
+
+/**
+ * 	This is a class to test Shiro session
+ * 
+ * @author vinsy
+ *
+ */
+@SuppressWarnings({"unused"})
+@RestController
+@RequestMapping("/shiro/session")
+public class ShiroSessionController {
+
+	@Autowired
+	private HttpServletRequest request;
+	@Autowired
+	private HttpServletResponse response;
+	
+	@RequestMapping(value = "/testSession", method = RequestMethod.GET)
+	public Map<String, Object> testSession() throws Exception {
+		
+		Map<String, Object> data = new HashMap<>();
+		Map<String, Object> result = new HashMap<>();
+		
+		// Get the principal and credential
+		String principal = request.getParameter("username");
+		String credentials = request.getParameter("password");
+		
+		// Do the Shiro login and get the login information
+		LoginInfo loginInfo = ShiroUtils.login("shiro/session/shiro.ini", principal, credentials);
+		
+		// If login succeeds
+		if (loginInfo.getIsLogin().booleanValue() == true) {
+			
+			// Get the subject
+			Subject currentUser = loginInfo.getSubject();
+			
+			// Get the principal
+			PrincipalCollection principals = currentUser.getPrincipals();
+			result.put("loginMsg", principals + " has logged-in");
+						
+			// Get the session of the subject
+			Session session = currentUser.getSession();
+			
+			// Get information of the session
+			result.put("sessionId", session.getId());
+			result.put("host", session.getHost());
+			result.put("startTimestamp", session.getStartTimestamp());
+			result.put("lastAccessTime", session.getLastAccessTime());
+			result.put("timeout", session.getTimeout());
+			
+			// Logout current user
+			currentUser.logout();
+			result.put("logoutMsg", principals + " has logged-out");
+						
+			// Return data
+			data.put("status", 1);
+			data.put("msg", "authentication succeeds");
+			data.put("result", result);
+			return data;
+			
+		// If login fails
+		} else {
+			
+			// Return data
+			result.put("errMsg", loginInfo.getMsg());
+			data.put("status", -1);
+			data.put("msg", "authentication fails");
+			data.put("result", result);
+			return data;
+		}
+	}
+}
