01e6a2beb50f1eec6b6fec6ede4c5907b58f40e4
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-06-28 18:35:24 +0800

    First-time Commit

--- SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/mybatis/provider/dao/MyBatisDAO.java
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/mybatis/provider/dao/MyBatisDAO.java
@@ -1,7 +1,10 @@
 package application.io.spring.technique.mybatis.provider.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import application.io.spring.technique.mybatis.api.model.MyBatis;
 
+@Mapper
 public interface MyBatisDAO {
 
 	public void insertSelective(MyBatis bean) throws Exception;
