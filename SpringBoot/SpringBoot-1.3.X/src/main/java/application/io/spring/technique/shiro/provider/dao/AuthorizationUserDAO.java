aa20567db649db28aeed9fae94b810c4bd79601f
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-07-04 23:22:55 +0800

    First-time Commit

---
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/provider/dao/AuthorizationUserDAO.java
@@ -0,0 +1,8 @@
+package application.io.spring.technique.shiro.provider.dao;
+
+import application.io.spring.technique.shiro.api.model.AuthorizationUser;
+import application.io.spring.technique.springboot.provider.dao.BaseDAO;
+
+public interface AuthorizationUserDAO extends BaseDAO<AuthorizationUser> {
+
+}
