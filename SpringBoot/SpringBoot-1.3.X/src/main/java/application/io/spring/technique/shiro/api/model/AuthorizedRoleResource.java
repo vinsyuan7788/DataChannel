4e5da1438eea3ad6c29366d1f8b201be48a74430
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-07-04 18:12:38 +0800

    First-time Commit

---
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/api/model/AuthorizedRoleResource.java
@@ -0,0 +1,8 @@
+package application.io.spring.technique.shiro.api.model;
+
+import application.io.spring.technique.springboot.utils.Identifiable;
+
+public class AuthorizedRoleResource extends Identifiable {
+
+	private static final long serialVersionUID = 1L;
+}
---
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/api/model/AuthorizedUserRole.java
@@ -0,0 +1,8 @@
+package application.io.spring.technique.shiro.api.model;
+
+import application.io.spring.technique.springboot.utils.Identifiable;
+
+public class AuthorizedUserRole extends Identifiable {
+
+	private static final long serialVersionUID = 1L;
+}
