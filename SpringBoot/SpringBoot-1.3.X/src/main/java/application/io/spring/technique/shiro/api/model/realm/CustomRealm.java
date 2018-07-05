0f92d09348c0d015e02bf1a7b7efe0da03891aa2
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-07-05 17:19:22 +0800

    First-time Commit

--- SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/api/model/realm/CustomRealm.java
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/shiro/api/model/realm/CustomRealm.java
@@ -3,9 +3,15 @@ package application.io.spring.technique.shiro.api.model.realm;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import application.io.spring.technique.shiro.api.model.AuthorizationUser;
+import application.io.spring.technique.shiro.api.service.AuthorizationUserService;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * 	This is a class to customize a realm for authentication and authorization
@@ -16,6 +22,9 @@ import org.apache.shiro.subject.PrincipalCollection;
  */
 public class CustomRealm extends AuthorizingRealm {
 
+	@Autowired
+	private AuthorizationUserService authorizationUserService;
+	
 	/**
 	 * 	This is a method to implement how a principal should be authorized
 	 */
@@ -31,7 +40,18 @@ public class CustomRealm extends AuthorizingRealm {
 	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		// TODO Auto-generated method stub
+		
+		String username = ((UsernamePasswordToken) token).getUsername();
+		
+		AuthorizationUser query = new AuthorizationUser();
+		query.setName(username);
+		
+		try {
+			AuthorizationUser authorizationUser = authorizationUserService.selectOneByQuery(query);
+		} catch (Exception e) {
+			
+		}
+		
 		return null;
 	}
 }
