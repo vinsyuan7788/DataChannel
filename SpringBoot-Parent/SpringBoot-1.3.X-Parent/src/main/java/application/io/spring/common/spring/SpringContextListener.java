2726a76347e239fe4c17fac6c4c0f5ccf1ed7aa9
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-07-08 12:36:53 +0800

    First-time Commit

--- SpringBoot-Parent/SpringBoot-1.3.X-Parent/src/main/java/application/io/spring/common/spring/SpringContextListener.java
+++ SpringBoot-Parent/SpringBoot-1.3.X-Parent/src/main/java/application/io/spring/common/spring/SpringContextListener.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
---
+++ SpringBoot/SpringBoot-1.3.X/src/main/java/application/io/spring/technique/springboot/utils/SpringContextListener1.java
@@ -0,0 +1,25 @@
+package application.io.spring.technique.springboot.utils;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ApplicationContextEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class SpringContextListener1 implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		
+		ApplicationContext springApplicationContext1 = event.getApplicationContext();
+		ApplicationContext parentApplicationContext1 = springApplicationContext1.getParent();
+		
+		ApplicationContext springApplicationContext2 = ((ApplicationContextEvent) event).getApplicationContext();
+		ApplicationContext parentApplicationContext2 = springApplicationContext2.getParent();
+		
+		System.out.println("=== SpringContextListener1"
+				+ " | springApplicationContext1: " + springApplicationContext1
+				+ " | parentApplicationContext1: " + parentApplicationContext1
+				+ " | springApplicationContext2: " + springApplicationContext2
+				+ " | parentApplicationContext2: " + parentApplicationContext2 + " ===");
+	}
+}
