45bdc0d7ebe45e7200f08bb9828184b0a96e6d19
vinsy <vinsy@LAPTOP-TQIIIOKJ>
2018-07-10 01:58:29 +0800

    First-time Commit

--- middleware/middleware-authorization/src/main/java/application/io/spring/business/authorization/api/service/AuthorizationUserService.java
+++ middleware/middleware-authorization/src/main/java/application/io/spring/business/authorization/api/service/AuthorizationUserService.java
@@ -7,8 +7,28 @@ import application.io.spring.business.authorization.api.vo.AuthorizationUserReso
 import application.io.spring.business.authorization.api.vo.AuthorizationUserRoleVo;
 import application.io.spring.core.base.api.service.BaseService;
 
-public interface AuthorizationUserService extends BaseService<AuthorizationUser> {
+public interface AuthorizationUserService {
 
+	public Boolean insertSelective(AuthorizationUser bean) throws Exception;
+	
+	public Boolean insertBatch(List<AuthorizationUser> beans) throws Exception;
+
+	public AuthorizationUser selectOneByQuery(AuthorizationUser query) throws Exception;
+	
+	public List<AuthorizationUser> selectAllByQuery(AuthorizationUser query) throws Exception;
+	
+	public List<AuthorizationUser> selectListByQuery(AuthorizationUser query, String orderby, Long limit, Long offset) throws Exception;
+
+	public List<AuthorizationUser> selectListByPrimaryKeyCollection(List<Long> primaryKeys) throws Exception;
+	
+	public Long getAllCountByQuery(AuthorizationUser query) throws Exception;
+
+	public Long getListCountByQuery(AuthorizationUser query, Long limit, Long offset) throws Exception;
+
+	public Boolean deleteByCondition(AuthorizationUser condition) throws Exception;
+
+	public Boolean updateByPrimaryKeySelective(AuthorizationUser bean) throws Exception;
+	
 	public List<AuthorizationUserRoleVo> selectAllUserRolesByName(AuthorizationUser query) throws Exception;
 	
 	public List<AuthorizationUserResourceVo> selectAllUserResourcesByName(AuthorizationUser query) throws Exception;
--- middleware/middleware-authorization/src/main/java/application/io/spring/business/authorization/provider/dao/AuthorizationUserDAO.java
+++ middleware/middleware-authorization/src/main/java/application/io/spring/business/authorization/provider/dao/AuthorizationUserDAO.java
@@ -8,7 +8,21 @@ import application.io.spring.business.authorization.api.vo.AuthorizationUserReso
 import application.io.spring.business.authorization.api.vo.AuthorizationUserRoleVo;
 import application.io.spring.core.base.provider.dao.BaseDAO;
 
-public interface AuthorizationUserDAO extends BaseDAO<AuthorizationUser> {
+public interface AuthorizationUserDAO {
+	
+	public void insertSelective(AuthorizationUser bean) throws Exception;
+	
+	public void insertBatch(List<AuthorizationUser> beans) throws Exception;
+
+	public List<AuthorizationUser> getPageableList(Map<String, Object> params) throws Exception;
+
+	public List<AuthorizationUser> selectByIdCollection(Map<String, Object> params) throws Exception;
+	
+	public Long getListCount(Map<String, Object> params) throws Exception;
+	
+	public void deleteByCondition(AuthorizationUser condition) throws Exception;
+	
+	public void updateByPrimaryKeySelective(AuthorizationUser bean) throws Exception;
 
 	public List<AuthorizationUserRoleVo> selectAllUserRolesByName(Map<String, Object> params) throws Exception;
 	
